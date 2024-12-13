428b0008d409145875f60a590f27760fe93329e9
Antoni Steglinski <antoni.steglinski@gmail.com>
2024-12-13 01:18:20 +0100

    Update top_pc.sv

--- repo/rtl/top_pc.sv
+++ repo/rtl/top_pc.sv
@@ -2,14 +2,16 @@ module top_pc (
     input  logic            clk,          // Clock signal
     input  logic            rst,          // Reset signal
     input  logic [31:0]     ImmOp,        // Immediate operand for branch
-    input  logic            PCSrc,        // Choice between branch and incremented PC
+    input  logic [31:0]     PCTargetE, // Branch/jump target from Execute stage
     input  logic            JALROn,
-    input  logic [31:0]     rs1,
+    input  logic [31:0]     RD1E,
+    input  logic            PCSrc,        // Choice between branch and incremented PC
+    input  logic [31:0]     PCE,
+    output logic [31:0]     inc_PC,        // PCPlus4
     output logic [31:0]     PC            // Current PC value
 );
 
     // Internal signals
-    logic [31:0] inc_PC;    // PC + 4
     logic [31:0] branch_PC; // PC + ImmOp
     logic [31:0] rs1_PC;
     logic [31:0] next_PC;   // Output from mux
@@ -18,10 +20,17 @@ module top_pc (
     // Assignments
     always_comb begin 
         inc_PC = PC + 4; // Normal increment
-        branch_PC = PC + ImmOp; // Branch increment
-        rs1_PC = rs1 + ImmOp;
+        branch_PC = PCE + ImmOp; // Use the branch/jump target calculated in the Execute stage
+        rs1_PC = RD1E + ImmOp;
     end
 
+    program_counter pc (
+        .clk(clk),
+        .next_PC(next_PC),
+        .PC(PC)
+    );
+
+    //Mux is in E block
     mux new_immop_mux (
         .in0(branch_PC),
         .in1(rs1_PC),
@@ -36,11 +45,6 @@ module top_pc (
         .out(next_PC)       
     );
 
-    program_counter pc (
-        .clk(clk),
-        .rst(rst),
-        .next_PC(next_PC),
-        .PC(PC)
-    );
+
 
 endmodule
